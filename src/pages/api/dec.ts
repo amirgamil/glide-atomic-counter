c6dccd8c101702f0a1796101d66936dade0c5b6b
David Siegel <djsiegel@gmail.com>
2021-09-13 08:56:26 -0700

    WIP

--- src/pages/api/dec.ts
+++ src/pages/api/dec.ts
@@ -2,11 +2,8 @@ import { allowCors } from "../../glide-next";
 import { connect } from "../../redis";
 
 export default allowCors(async (req, res) => {
-  const {
-    params: {
-      counter: { value: counter },
-    },
-  } = req.body;
+  const counter =
+    (req.query.counter as string) ?? req.body?.params?.counter?.value ?? "";
   const client = await connect();
   const count = await client.decrement(counter);
   await client.disconnect();
---
+++ src/pages/counter.tsx
@@ -0,0 +1,45 @@
+import { useRouter } from "next/dist/client/router";
+import { useEffect, useState } from "react";
+
+function CounterPage() {
+  const router = useRouter();
+  const counter = router.query.counter ?? "root";
+
+  const [count, setCount] = useState<number | undefined>(undefined);
+
+  useEffect(() => {
+    fetch(`/api/peek?counter=${counter}`)
+      .then((x) => x.json())
+      .then((x) => setCount(x.count));
+  });
+
+  async function inc() {
+    const response = await fetch(`/api/inc?counter=${counter}`).then((x) =>
+      x.json()
+    );
+    setCount(response.count);
+  }
+
+  async function dec() {
+    const response = await fetch(`/api/dec?counter=${counter}`).then((x) =>
+      x.json()
+    );
+    setCount(response.count);
+  }
+
+  return (
+    <div className="flex flex-col space-y-4 items-center">
+      <h1 className="text-5xl mb-10">{count}</h1>
+      <div className="grid grid-cols-2 gap-5">
+        <button className="rounded-lg border shadow p-4" onClick={dec}>
+          Decrement
+        </button>
+        <button className="rounded-lg border shadow p-4" onClick={inc}>
+          Increment
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default CounterPage;
